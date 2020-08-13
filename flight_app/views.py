76562cc6724291e9e74fca014c993e9356f1f6c4
Orchyl Salmasan <salmasan.orchyl@gmail.com>
2020-08-13 20:15:07 +0800

    Configured viewsets on Views

--- flight_app/views.py
+++ flight_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from flight_app.models import Flight, Passenger, Reservation
+from flight_app.serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+from rest_framework import viewsets
+
 
 # Create your views here.
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
