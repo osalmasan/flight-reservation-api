795e0000fe94810e50a5eb08b328bee6c489e7d8
Orchyl Salmasan <salmasan.orchyl@gmail.com>
2020-08-13 21:06:34 +0800

    Modified Serializers to ModelSerializers

--- flight_app/models.py
+++ flight_app/models.py
@@ -9,6 +9,8 @@ class Flight(models.Model):
     date_of_departure = models.DateField()
     eta_departure = models.TimeField()
 
+    def __str__(self):
+        return self.flight_number + " " + self.op_airline
 
 class Passenger(models.Model):
     firstname = models.CharField(max_length=20)
@@ -16,6 +18,9 @@ class Passenger(models.Model):
     middlename = models.CharField(max_length=20)
     email = models.EmailField(max_length=20)
     phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
 
 
 class Reservation(models.Model):
--- flight_app/serializers.py
+++ flight_app/serializers.py
@@ -1,20 +1,18 @@
-from abc import ABC
-
 from rest_framework import serializers
-from flight_app.models import Flight, Passenger,Reservation
+from flight_app.models import Flight, Passenger, Reservation
 
 
-class FlightSerializer(serializers.Serializer):
+class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
 
-class PassengerSerializer(serializers.Serializer):
+class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Passenger
         fields = '__all__'
 
-class ReservationSerializer(serializers.Serializer):
+class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
