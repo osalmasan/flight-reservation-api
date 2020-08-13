1e7691798dcda41f2f4b5b8258f3add64db776b0
Orchyl Salmasan <salmasan.orchyl@gmail.com>
2020-08-13 20:08:08 +0800

    Configured db relationship/entities

--- flight_app/models.py
+++ flight_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    flight_number = models.CharField(max_length=10)
+    op_airline = models.CharField(max_length=10)
+    departure_city = models.CharField(max_length=10)
+    arrival_city = models.CharField(max_length=10)
+    date_of_departure = models.DateField()
+    eta_departure = models.TimeField()
+
+
+class Passenger(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    middlename = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    phone = models.CharField(max_length=10)
+
+
+class Reservation(models.Model):
+    flight = models.OneToOneField(Flight, on_delete=models.CASCADE)
+    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
